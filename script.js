6e402c98523cea9ef5caf8067053e0e26fc28b88
Glitch (hello-webpage) <none>
2019-02-15 19:02:13 +0000

    🐞⛷ Checkpoint ./script.js:234254/3908 ./index.html:234254/59

--- script.js
+++ script.js
@@ -1,5 +1,23 @@
-/* If you're feeling fancy you can add interactivity 
-    to your site with Javascript */
 
-// prints "hi" in the browser's dev tools console
-console.log('hi');
+navigator.mediaDevices.getUserMedia({
+  audio: false,
+  video: {
+    width: 1280,
+    height: 720
+  }
+}).then(function(stream) {
+  let video = document.createElement("video");
+  document.body.appendChild(video);
+  video.srcObject = stream;
+  video.onloadedmetadata = function(e) {
+    video.play();
+  }
+  
+  let canvas = document.querySelector('canvas');
+  let ctx = canvas.getContext('2d');
+  function frame() {
+    ctx.drawImage(video, 0, 0, video.videoWidth, video.videoHeight, 0, 0, canvas.width, canvas.height);
+    requestAnimationFrame(frame);
+  }
+  frame();
+}).catch(e => console.error(e));
