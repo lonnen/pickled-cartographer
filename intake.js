9a9c59b046298af523f9bb77afa436230320593b
Glitch (hello-webpage) <none>
2019-02-24 07:00:40 +0000

    😋👩‍❤️‍💋‍👩 Checkpoint ./intake.js:234254/1175 ./watch.json:234254/7

--- intake.js
+++ intake.js
@@ -12,22 +12,26 @@ img.addEventListener('load', function () {
   let sobel = new ImageData(256, 256);
   let data = id.data;
 
-  const lum = (id, i) => (id[i] * .299 + id[i + 1] * .587 + id[i + 2] * .114) | 0;
+  const lum = (id, i) => (id[i] * .299 + id[i + 1] * .587 + id[i + 2] * .114);
   const idx = (x, y) => y * 256 * 4 + x * 4;
 
   // the meat
+  let min = Infinity;
+  let max = 0;
   for (let i = 0; i < data.length; i+=4) {
     let x = (i / 4) % 256;
     let y = (i / 4) / 256 | 0;
     sobel.data[i+3] = 255;
     if (x > 0 && x < 255 && y > 0 && y < 255) {
       let l = lum(data, i);
-      let val = (
+      let val = Math.floor(Math.abs(
         -1 * lum(data, idx(x - 1, y - 1)) + 1 * lum(data, idx(x + 1, y - 1)) +
         -2 * lum(data, idx(x - 1, y)) + 2 * lum(data, idx(x + 1, y)) +
         -1 * lum(data, idx(x - 1, y + 1)) + 1 * lum(data, idx(x + 1, y + 1))
-      );
-      sobel.data[i] = sobel.data[i + 1] = sobel.data[i + 2] = Math.abs(val) * 5;
+      ));
+      if (val > max) max = val;
+      if (val < min) min = val;
+      sobel.data[i] = sobel.data[i + 1] = sobel.data[i + 2] = val;
     } else {
       sobel.data[i] = sobel.data[i + 1] = sobel.data[i + 2] = 0;
     }
@@ -48,20 +52,19 @@ img.addEventListener('load', function () {
   // }
 
   let ct = contours(sobel);
-  
- if (ct.length) {
+  ctx.putImageData(sobel, 0, 0);
+
+  console.log(ct);
+  if (ct.length) {
     let contour = ct[0];
     for (let i = 1; i < ct.length; i++) {
       if (ct[i].length > contour.length) {
         contour = ct[i];
       }
     }
-    ctx.clearRect(0, 0, canvas.width, canvas.height);
-    ctx.strokeRect(0, 0, 100, 100);
     ctx.beginPath();
     ctx.lineWidth = 3;
     ctx.strokeStyle = '#0f0';
-    ctx.fillStyle= '#rgba(0, 255, 0, .5)';
 
     let minX = Infinity;
     let minY = Infinity;
@@ -69,22 +72,18 @@ img.addEventListener('load', function () {
     let maxY = -Infinity;
 
     for (let pos of contour) {
-      let post = (i / 4) % 256;
-      let y = (i / 4) / 256 | 0;
+      let x = pos % 256;
+      let y = pos / 256 | 0;
       if (x < minX) minX = x;
       if (y < minY) minY = y;
       if (x > maxX) maxX = x;
       if (y > maxY) maxY = y;
       ctx.lineTo(x, y);
     }
-    ctx.fill();
-    ctx.strokeRect(minX, minY, maxX-minX, maxY-minY);
-    sigCtx.drawImage(video, minX - 8, minY - 8, maxX - minX + 16, maxY - minY + 16, 0, 0, signatureSize, signatureSize);
+    ctx.stroke();
+    // ctx.strokeRect(minX, minY, maxX-minX, maxY-minY);
   }
 
-  
-  console.log(ct);
-
-  ctx.putImageData(sobel, 0, 0);
 });
+
 img.src="https://cdn.glitch.com/4945918e-6ab3-4a5c-8549-71e001d5a0e8%2Fb9-crescent-isle.png?1550987729191";
