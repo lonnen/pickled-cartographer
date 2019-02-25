faf5d278c2d3b4fca68ced28367b3be954a4e08c
Glitch (hello-webpage) <none>
2019-02-25 16:22:16 +0000

    🍃👹 Checkpoint ./script.js:234254/17

--- script.js
+++ script.js
@@ -75,11 +75,11 @@ function start(stream) {
       data[i + 2] = yellow;
     }
     
-    let { min, max, sobel } = sobelFilter(id);
+    let { min, max, sobel } = normalize(sobelFilter(id));
     
     ctx.putImageData(sobel, 0, 0);
     
-    let threshold = .5;
+    let threshold = .2;
     let threshValue = (max - min) * threshold + min;
     for (let i = 0; i < data.length; i+=4) {
       if (sobel.data[i] > threshValue) {
